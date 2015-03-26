ecee63fd55b5136d1fbb445402bf2ab1c4909877
md <markusdrenger+github@gmail.com>
2015-03-26 19:21:42 +0100

    parsed jetzt auch die Anlagenübersicht pro Tagesordnungspunkt

--- session.py
+++ session.py
@@ -24,7 +24,7 @@ class Form(object):
 
 class RubinScraper(object):
     def __init__(self, db_connection_string, domain='darmstadt'):
-        self.base_url = 'http://'+domain+'.more-rubin1.de/'
+        self.base_url = 'http://' + domain + '.more-rubin1.de/'
 
         self.db = dataset.connect(db_connection_string)
         t_lastaccess = self.db['updates']
@@ -40,17 +40,42 @@ class RubinScraper(object):
         psoup = BeautifulSoup(html)
         text = psoup.find('div', {'class': 'aktualisierung'}).get_text()
         last_website_update = text[len('Letzte Aktualisierung am:'):]
-        websitedatetime = datetime.datetime.strptime(last_website_update,"%d.%m.%Y, %H:%M")
-        db_datetime=""
+        websitedatetime = datetime.datetime.strptime(last_website_update, "%d.%m.%Y, %H:%M")
+        db_datetime = ""
         query = self.db.query("SELECT max(scraped_at) as lastaccess from updates")
         for row in query:
             db_datetime = row['lastaccess']
         self.db['updates'].insert(dict(scraped_at=datetime.datetime.now()))
         if db_datetime is None:
             return True
-        if datetime.datetime.strptime(db_datetime[:16],"%Y-%m-%d %H:%M") < websitedatetime:
+        if datetime.datetime.strptime(db_datetime[:16], "%Y-%m-%d %H:%M") < websitedatetime:
             return True
         return False
+
+
+    def scrapeAttachmentsPage(self, sessionID, agenda_item_id, attachmentsPageURL):
+        print("scrape Attachment "+attachmentsPageURL)
+        html = requests.get(attachmentsPageURL).text
+        soup = BeautifulSoup(html)
+        txt = soup.get_text()
+
+        if "Auf die Anlage konnte nicht zugegriffen werden oder Sie existiert nicht mehr." in txt:
+            print("Zu TOP "+agenda_item_id+" fehlt mindestens eine Anlage")
+            errortable = self.db['404attachments']
+            errortable.insert(dict(agenda_item_id= agenda_item_id, attachmentsPageURL= attachmentsPageURL))
+
+        for forms in soup.find_all('form'):
+            title = forms.get_text()
+            values = list()
+            for val in forms.find_all('input', {'type': 'hidden'}):
+                values.append([val['name'], val['value']])
+
+            form = Form(forms['action'],values)
+            url = self.base_url + form.toURL()
+
+            tab = self.db['attachments']
+            tab.insert(dict(sid= sessionID, agenda_item_id= agenda_item_id, attachment_title= title, attachment_file_url= url))
+
 
     def getSIDsOfMeetings(self):
         # TODO: the dates should be automatically generated
@@ -165,11 +190,14 @@ class RubinScraper(object):
                 gesamtID = top[4][10:top[4].index(',')]
 
             tab = self.db['agenda']
+            attachment_link=top[6]
             tab.insert(
                 dict(sid=sid, status=top[0], topnumber=top[1], column3=top[2], details_link=top[3], title_full=top[4],
                      document_link=top[
-                         5], attachment_link=top[6], decision_link=top[7], column9=top[8], column10=top[9], year=jahr,
+                         5], attachment_link=attachment_link, decision_link=top[7], column9=top[8], column10=top[9], year=jahr,
                      billnumber=vorlnr, billid=gesamtID, position=count))
+            if "http://" in attachment_link:
+                self.scrapeAttachmentsPage(sid,gesamtID,attachment_link)
 
 
 if __name__ == '__main__':
