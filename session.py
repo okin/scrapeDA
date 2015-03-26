f4f4b2d1bd04d5626628f5f85ed6817101242a61
Niko Wenselowski <der@nik0.de>
2015-03-26 09:32:31 +0100

    Got rid of global variables - we now have a class.

--- session.py
+++ session.py
@@ -9,13 +9,6 @@ import requests
 import sqlalchemy
 from bs4 import BeautifulSoup
 
-base_url = 'http://darmstadt.more-rubin1.de/'
-
-# setup DB
-db = dataset.connect('sqlite:///darmstadt.db')
-t_lastaccess = db['lastscrape']
-t_lastaccess.create_column('scraped_at', sqlalchemy.DateTime)
-
 
 class Form(object):
 
@@ -31,133 +24,147 @@ class Form(object):
         return "{}?{}".format(self.action, '&'.join(parameters))
 
 
-def getSIDsOfMeetings():
-    scrape_from = '01.01.2006'
-    scrape_to = '31.01.2006'
-    search_parameters = ("recherche.php?suchbegriffe=&select_gremium=&"
-                         "datum_von={start}&datum_bis={end}&"
-                         "startsuche=Suche+starten")
-    search_parameters = search_parameters.format(start=scrape_from,
-                                                 end=scrape_to)
+class RubinScraper(object):
+    def __init__(self, db_connection_string, base_url='http://darmstadt.more-rubin1.de/'):
+        self.base_url = base_url
 
-    starturl = urljoin(base_url, search_parameters)
-    entry = 0  # set for first run
-    SIDs = set()
-    notempty = 1
-    while notempty > 0:
-        # prevent infinite loop
-        notempty = 0
-        site_content = requests.get(starturl + "&entry=" + str(entry - 1)).text
-        table = BeautifulSoup(site_content).find('table', {"width": "100%"})
+        self.db = dataset.connect('sqlite:///darmstadt.db')
+        t_lastaccess = self.db['lastscrape']
+        t_lastaccess.create_column('scraped_at', sqlalchemy.DateTime)
 
-        for inputs in table.find_all('input', {"name": "sid"}):
-            sid = inputs["value"]
-            if sid not in SIDs:
-                SIDs.add(inputs["value"])
-                entry = entry + 1
-                notempty = notempty + 1
-                yield sid
+    def scrape(self):
+        for sid in self.getSIDsOfMeetings():
+            self.getSession(sid)
 
+    def getSIDsOfMeetings(self):
+        # TODO: the dates should be automatically generated
+        scrape_from = '01.01.2006'
+        scrape_to = '31.01.2006'
+        search_parameters = ("recherche.php?suchbegriffe=&select_gremium=&"
+                             "datum_von={start}&datum_bis={end}&"
+                             "startsuche=Suche+starten")
+        search_parameters = search_parameters.format(start=scrape_from,
+                                                     end=scrape_to)
 
-def getSession(sid):
-    if not sid:
-        raise RuntimeError("Missing session ID.")
-    print(sid)
+        starturl = urljoin(self.base_url, search_parameters)
+        entry = 0  # set for first run
+        SIDs = set()
+        notempty = 1
+        while notempty > 0:
+            # prevent infinite loop
+            notempty = 0
+            site_content = requests.get(starturl + "&entry=" + str(entry - 1)).text
+            table = BeautifulSoup(site_content).find('table', {"width": "100%"})
 
-    target_url = urljoin(base_url, "sitzungen_top.php")
-    site_content = requests.get(target_url, params={"sid": sid}).text
-    soup = BeautifulSoup(site_content)
+            for inputs in table.find_all('input', {"name": "sid"}):
+                sid = inputs["value"]
+                if not sid:
+                    continue
 
-    session = {'sid': sid}
-    session['title'] = soup.find('b', {'class': 'Suchueberschrift'}).get_text(
-    )
-    # METADATEN
-    table = soup.find('div', {'class': 'InfoBlock'}).find('table')
-    values = parseTable(table)
-
-    for row in values:
-        if row[0] == "Termin: ":
-            datum = row[1]
-            if len(datum) == len("29.11.2006, 15:00 Uhr - 15:45 Uhr"):
-                beginn = datetime.datetime.strptime(datum[12:17], "%H:%M")
-                ende = datetime.datetime.strptime(datum[24:29], "%H:%M")
-                delta = ende - beginn
-                session['start'] = str(beginn)
-                session['end'] = str(ende)
-                session['date'] = datum[0:10]
-                session['duration'] = str(delta.seconds / 60)
-        elif row[0] == "Raum: ":
-            session['location'] = str(row[1])
-        elif row[0] == "Gremien: ":
-            session['body'] = str(row[1])
-        t_sessions = db['sessions']
-        print(session)
-        t_sessions.insert(session)
-
-    for tab in soup.find_all('table'):
-        tr = int(len(tab.find_all('tr')))
-        td = int(len(tab.find_all('td')))
-        if td > 9 * tr:
-            tops = parseTable(tab)
-            parseTOPs(sid, tops)
+                if sid not in SIDs:
+                    SIDs.add(inputs["value"])
+                    entry = entry + 1
+                    notempty = notempty + 1
+                    yield sid
 
 
-def parseTable(table):
-    values = list()
-    for TRs in table.find_all('tr'):
-        row = list()
-        for TDs in TRs.find_all('td'):
-            if TDs.form != None:
-                url = extractHiddenFormURL(TDs)
-                row.append(url)
-            else:
-                row.append(TDs.get_text())
-        values.append(row)
-    return values
+    def getSession(self, sid):
+        if not sid:
+            raise RuntimeError("Missing session ID.")
+        print(sid)
 
+        target_url = urljoin(self.base_url, "sitzungen_top.php")
+        site_content = requests.get(target_url, params={"sid": sid}).text
+        soup = BeautifulSoup(site_content)
 
-def extractHiddenFormURL(td):
-    form = Form(td.form['action'])
+        session = {'sid': sid}
+        session['title'] = soup.find('b', {'class': 'Suchueberschrift'}).get_text(
+        )
+        # METADATEN
+        table = soup.find('div', {'class': 'InfoBlock'}).find('table')
+        values = self.parseTable(table)
 
-    for tag in td.form.find_all('input', {'type': 'hidden'}):
-        form.values.append((tag['name'], tag['value']))
+        for row in values:
+            if row[0] == "Termin: ":
+                datum = row[1]
+                if len(datum) == len("29.11.2006, 15:00 Uhr - 15:45 Uhr"):
+                    beginn = datetime.datetime.strptime(datum[12:17], "%H:%M")
+                    ende = datetime.datetime.strptime(datum[24:29], "%H:%M")
+                    delta = ende - beginn
+                    session['start'] = str(beginn)
+                    session['end'] = str(ende)
+                    session['date'] = datum[0:10]
+                    session['duration'] = str(delta.seconds / 60)
+            elif row[0] == "Raum: ":
+                session['location'] = str(row[1])
+            elif row[0] == "Gremien: ":
+                session['body'] = str(row[1])
+            t_sessions = self.db['sessions']
+            print(session)
+            t_sessions.insert(session)
 
-    return urljoin(base_url, form.toURL())
+        for tab in soup.find_all('table'):
+            tr = int(len(tab.find_all('tr')))
+            td = int(len(tab.find_all('td')))
+            if td > 9 * tr:
+                tops = self.parseTable(tab)
+                self.parseTOPs(sid, tops)
 
+    def parseTable(self, table):
+        values = list()
+        for TRs in table.find_all('tr'):
+            row = list()
+            for TDs in TRs.find_all('td'):
+                if TDs.form != None:
+                    url = self.extractHiddenFormURL(TDs)
+                    row.append(url)
+                else:
+                    row.append(TDs.get_text())
+            values.append(row)
+        return values
 
-def parseTOPs(sid, tops):
-    count = 0
-    for top in tops:
-        count = count + 1
-        vorlnr = ""
-        gesamtID = ""
-        jahr = ""
-        if '[Vorlage: ' in top[4]:
-            if '[Vorlage: SV-' in top[4]:
-                jahr = top[4][len("Vorlage: SV-") + 1:len("Vorlage: SV-") + 5]
-                vorlnr = top[4][
-                    len("Vorlage: SV-") + 6:len("Vorlage: SV-") + 10]
-            else:
-                jahr = top[4][len("Vorlage: ") + 1:len("Vorlage: ") + 5]
-                vorlnr = top[4][len("Vorlage: ") + 6:len("Vorlage: ") + 10]
-            gesamtID = top[4][10:top[4].index(',')]
+    def extractHiddenFormURL(self, td):
+        form = Form(td.form['action'])
 
-        tab = db['agenda']
-        tab.insert(dict(sid=sid, status=top[0], topnumber=top[1], column3=top[2], details_link=top[3], title_full=top[4], document_link=top[
-                   5], attachment_link=top[6], decision_link=top[7], column9=top[8], column10=top[9], year=jahr, billnumber=vorlnr, billid=gesamtID, position=count))
+        for tag in td.form.find_all('input', {'type': 'hidden'}):
+            form.values.append((tag['name'], tag['value']))
+
+        return urljoin(self.base_url, form.toURL())
+
+    def parseTOPs(self, sid, tops):
+        count = 0
+        for top in tops:
+            count = count + 1
+            vorlnr = ""
+            gesamtID = ""
+            jahr = ""
+            if '[Vorlage: ' in top[4]:
+                if '[Vorlage: SV-' in top[4]:
+                    jahr = top[4][len("Vorlage: SV-") + 1:len("Vorlage: SV-") + 5]
+                    vorlnr = top[4][
+                        len("Vorlage: SV-") + 6:len("Vorlage: SV-") + 10]
+                else:
+                    jahr = top[4][len("Vorlage: ") + 1:len("Vorlage: ") + 5]
+                    vorlnr = top[4][len("Vorlage: ") + 6:len("Vorlage: ") + 10]
+                gesamtID = top[4][10:top[4].index(',')]
+
+            tab = self.db['agenda']
+            tab.insert(dict(sid=sid, status=top[0], topnumber=top[1], column3=top[2], details_link=top[3], title_full=top[4], document_link=top[
+                       5], attachment_link=top[6], decision_link=top[7], column9=top[8], column10=top[9], year=jahr, billnumber=vorlnr, billid=gesamtID, position=count))
 
 
 if __name__ == '__main__':
-    for sid in getSIDsOfMeetings():
-        getSession(sid)
+    scraper = RubinScraper('sqlite:///darmstadt.db')
+    scraper.scrape()
 
-    # getSession("ni_2006-HFA-7")
-    rest = db['sessions'].all()
+    scraper.getSession("ni_2006-HFA-7")
+
+    rest = scraper.db['sessions'].all()
     dataset.freeze(rest, format='json', filename='da-sessions.json')
-    rest = db['sessions'].all()
+    rest = scraper.db['sessions'].all()
     dataset.freeze(rest, format='csv', filename='da-sessions.csv')
 
-    rest = db['agenda'].all()
+    rest = scraper.db['agenda'].all()
     dataset.freeze(rest, format='json', filename='da-agenda.json')
-    rest = db['agenda'].all()
+    rest = scraper.db['agenda'].all()
     dataset.freeze(rest, format='csv', filename='da-agenda.csv')
